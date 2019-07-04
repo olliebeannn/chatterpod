d05cc029d4b5d613f495a65a5075956a92e6014b
Ollie Khakwani <okhakwani@gmail.com>
2019-07-03 22:47:06 -0400

    more setup for postgres db, no models created yet

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -1,26 +1,32 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+require('dotenv').config();
+
+module.exports = {
+  // If using onine database
+  // development: {
+  //   use_env_variable: 'DATABASE_URL'
+  // },
+
+  development: {
+    database: 'chatterpod',
+    username: 'postgres',
+    password: null,
+    host: '127.0.0.1',
+    dialect: 'postgres'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+
+  test: {
+    database: 'chatterpod_test',
+    username: 'postgres',
+    password: null,
+    host: '127.0.0.1',
+    dialect: 'postgres'
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
   }
-}
+};
