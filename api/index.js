31ee462d37560405fb13c5dd5bf864599f1ee506
Ollie Khakwani <okhakwani@gmail.com>
2019-07-04 23:17:17 -0400

    still cannot get /auth/google to go to login page

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-// import passport from 'passport';
-// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import passport from 'passport';
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // import database from './server/src/models';
 import userRoutes from './server/routes/UserRoutes';
@@ -10,50 +10,35 @@ import userRoutes from './server/routes/UserRoutes';
 const PORT = process.env.PORT | 5000;
 const app = express();
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.googleClientID,
-//       clientSecret: process.env.googleClientSecret,
-//       callbackURL: '/auth/google/callback'
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       console.log(profile);
-//
-//       const existingUser = await database.User.create({
-//         googleId: profile.id,
-//         email: profile.emails[0].value
-//       });
-//
-//       done(null, user);
-//     }
-//   )
-// );
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.googleClientID,
+      clientSecret: process.env.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-app.use('/users', userRoutes);
+app.get('/auth/google', (req, res) => {
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  });
+});
 
-// app.get('/auth/google', (req, res) => {
-//   passport.authenticate('google', {
-//     scope: ['profile', 'email']
-//   });
-// });
-//
-// app.get(
-//   '/auth/google/callback',
-//   passport.authenticate('google'),
-//   (req, res) => {
-//     res.redirect('/');
-//   }
-// );
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`App up on port ${PORT}`);
