4aaf38359cbcc5276715f5dcb4143435f0e9c975
Ollie Khakwani <okhakwani@gmail.com>
2019-08-03 19:02:19 -0400

    attempted fix for heroku to serve index.html

--- api/index.js
+++ api/index.js
@@ -42,7 +42,9 @@ if (process.env.NODE_ENV === 'production') {
   // Make Express serve index.html if it doesn't recognise route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
   });
 }
 
