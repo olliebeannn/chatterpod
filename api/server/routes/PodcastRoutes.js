250f211e8e2d84e5e54900c4ff459605f6d448c6
Ollie Khakwani <okhakwani@gmail.com>
2019-08-06 22:37:02 -0400

    added v basic middleware to validate user is logged in; need better UI response instead of 401 in chrome console

--- api/server/routes/PodcastRoutes.js
+++ api/server/routes/PodcastRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import PodcastController from '../controllers/PodcastController';
 
+import requireLogin from '../middlewares/requireLogin';
+
 const router = Router();
 
 // Get all podcasts saved in DB - not user-facing
 router.get('/', PodcastController.getAllPodcasts);
 // Get all saved podcasts for this users
-router.get('/saved', PodcastController.getSavedPodcasts);
+router.get('/saved', requireLogin, PodcastController.getSavedPodcasts);
 
 // Get top 20 podcasts from Listen API
 router.get('/top', PodcastController.getTopPodcasts);
