2c6aeb8e956c937818dbd76429f7fa0a1559def1
Ollie Khakwani <okhakwani@gmail.com>
2019-08-07 15:14:11 -0400

    fixed typo in createEpisode migration

--- api/server/src/migrations/20190807180443-create-episode.js
+++ api/server/src/migrations/20190807180443-create-episode.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('episodes', {
-      epsiodeId: {
+      episodeId: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.STRING
